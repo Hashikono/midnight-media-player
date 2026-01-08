5d54fc51d1c76b234f578295f5c352a94ed5cde0
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2026-01-08 05:09:00 +0100

    Again

--- Database.java
+++ Database.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
