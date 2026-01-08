5a8e05b714267053305469f1bd61ae77270c8f03
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2026-01-08 04:55:38 +0100

    Added Popup Menu for Adding Media

--- MediaAddingMenu.java
+++ MediaAddingMenu.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.io.File;
@@ -13,19 +14,33 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import models.Media;
+
 public class MediaAddingMenu {
     public static JDialog mediaAddingDialog;
     public static String tryingThis;
 
     
-    private static JTextField fileField = new JTextField(20);
+    private static JTextField pathField = new JTextField(20);
     private static JTextField nameField = new JTextField(20);
     private static JTextField extField = new JTextField(6);
     private static JTextField authorField = new JTextField(20);
     private static JTextField albumField = new JTextField(20);
 
+    private static Media result = null;
+
 
     public static JDialog OpenMediaAddingMenu(JFrame parent) {
+
+        pathField.setText("");
+        nameField.setText("");
+        extField.setText("");
+        authorField.setText("");
+        albumField.setText("");
+
+
+
+
         mediaAddingDialog = new JDialog(parent, "Add Media", true);
 
         JPanel formPanel = new JPanel(new GridBagLayout());
@@ -43,7 +58,7 @@ public class MediaAddingMenu {
 
         // File row
         formPanel.add(new JLabel("File:"), gbc(c, 0, y));
-        formPanel.add(fileField, gbc(c, 1, y));
+        formPanel.add(pathField, gbc(c, 1, y));
         JButton browse = new JButton("Browse...");
         browse.addActionListener(e -> openFileSelector());
         formPanel.add(browse, gbc(c, 2, y++));
@@ -65,41 +80,53 @@ public class MediaAddingMenu {
         formPanel.add(albumField, gbc(c, 1, y++, 2));
 
         // Buttons
-        // JPanel buttons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         JButton ok = new JButton("OK");
         JButton cancel = new JButton("Cancel");
 
-        // ok.addActionListener(e -> submit());
-        // cancel.addActionListener(e -> dispose());
+        buttons.add(ok);
+        buttons.add(cancel);
+
+        ok.addActionListener(e -> submit());
+        cancel.addActionListener(e -> mediaAddingDialog.dispose());
 
         // buttons.add(ok);
         // buttons.add(cancel);
 
         c.gridwidth = 3;
-        // add(buttons, gbc(c, 0, y));
-
-
-        // openFileSelector();
-
-        // JLabel label = new JLabel(tryingThis, SwingConstants.CENTER);
-        // JButton ok = new JButton("OK");
-
-        // ok.addActionListener(e -> mediaAddingDialog.dispose()); // closes dialog
-
-        // mediaAddingDialog.add(label, BorderLayout.CENTER);
-        // mediaAddingDialog.add(ok, BorderLayout.SOUTH);
 
         mediaAddingDialog.setSize(400, 200);
         mediaAddingDialog.add(formPanel, BorderLayout.CENTER);
+        mediaAddingDialog.add(buttons, BorderLayout.SOUTH);
 
         return mediaAddingDialog;
+    }
+
+    private static void submit() {
+        Media info = new Media(
+            pathField.getText(),
+            nameField.getText(),
+            extField.getText(),
+            authorField.getText(),
+            albumField.getText()
+        );
+        
+
+        result = info;
+        mediaAddingDialog.dispose();
     }
 
     public static File openFileSelector()
     {
         JFileChooser chooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter(
-            "JPG & GIF Images", "jpg", "gif");
+            "Audio & Video Files",
+            // Audio
+            "mp3", "wav", "flac", "ogg", "aac", "m4a",
+            // Video
+            "mp4", "mkv", "avi", "mov", "webm"
+        );
+        
         chooser.setFileFilter(filter);
         int returnVal = chooser.showOpenDialog(mediaAddingDialog);
         if(returnVal == JFileChooser.APPROVE_OPTION) {
@@ -109,8 +136,21 @@ public class MediaAddingMenu {
             tryingThis = chooser.getSelectedFile().getAbsolutePath();
         }
 
-
+        pathField.setText(chooser.getSelectedFile().getAbsolutePath());
+        nameField.setText(chooser.getSelectedFile().getName());
+        extField.setText(getExtensionOf(chooser.getSelectedFile().getName()));
         return chooser.getSelectedFile();
+    }
+
+    public static String getExtensionOf(String fileName) {
+        String extension = "";
+
+        int dot = fileName.lastIndexOf('.');
+        if(dot > 0 && dot < fileName.length() - 1) {
+            extension = fileName.substring(dot + 1).toLowerCase();
+        }
+
+        return extension;
     }
 
     
