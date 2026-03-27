172ce975715b060db4b78a9bf5b1719c87c5e62f
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2026-03-27 14:22:39 -0400

    Made basic Song Item

--- SongItem.java
+++ SongItem.java
@@ -1,15 +1,62 @@
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import models.Media;
 
 public class SongItem extends JPanel {
-    private JLabel songTitle = new JLabel();
-    private JLabel songAuthor = new JLabel();
+    private JPanel mainSongDetails;
+    private JLabel songImage;
+    private JLabel songTitle;
+    private JLabel songAuthor;
 
-    public SongItem() {
+    // JPanel panel = new JPanel(new GridBagLayout());
+    // GridBagConstraints gbc = new GridBagConstraints();
+
+    // gbc.fill = GridBagConstraints.BOTH;
+    // gbc.gridy = 0;
+
+    // // Left (30%)
+    // gbc.gridx = 0;
+    // gbc.weightx = 0.3;
+    // panel.add(leftPanel, gbc);
+
+    // // Right (70%)
+    // gbc.gridx = 1;
+    // gbc.weightx = 0.7;
+    // panel.add(rightPanel, gbc);
+
+    public SongItem(Media data) {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(songTitle);
-        add(songAuthor);
+        mainSongDetails = new JPanel();
+        mainSongDetails.setLayout(new FlowLayout(FlowLayout.LEADING, 3, 0));
+        
+        songImage = new JLabel(new ImageIcon("TempSongImage.png"));
+        songTitle = new JLabel(data.name);
+        songAuthor = new JLabel(data.author);
+
+
+        // mainSongDetails.add(songImage);
+        mainSongDetails.add(songTitle);
+        mainSongDetails.add(songAuthor);
+
+        add(mainSongDetails);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        Container parent = getParent();
+
+        if (parent != null) {
+            int width = parent.getWidth();
+            return new Dimension(width, 35);
+        }
+
+        return super.getPreferredSize();
     }
 }
--- SongsMenu.java
+++ SongsMenu.java
@@ -1,22 +1,74 @@
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import models.Media;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongsMenu extends JPanel {
+    private JPanel MusicListContainer;
+    private List<Media> allSongs;
+    private List<SongItem> allListedMedia = new ArrayList<>();
+
+    public void CreateMediaList()
+    {
+        try {
+            allSongs = Database.getAllMedia();
+
+            for(Media song : allSongs)
+            {
+                SongItem newListItem = new SongItem(song);
+                allListedMedia.add((SongItem) newListItem);
+                MusicListContainer.add(newListItem);
+                newListItem.setOpaque(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public SongsMenu()
     {
-        setLayout(new BorderLayout());
-        setBorder(new EmptyBorder(12, 12, 12, 12));
+        setSize(1000, 10);
+        setLayout(new BorderLayout(10, 7));
+        setBorder(new EmptyBorder(12, 12, 12, 20));
         setBackground(ColorScheme.DARK_BG);
         
         var title = new JLabel("Songs");
         title.setFont(new Font("Segoe UI", Font.PLAIN, 25));
+        title.setForeground(ColorScheme.TEXT_COLOR);
         title.setHorizontalAlignment(SwingConstants.LEADING);
 
+        MusicListContainer = new JPanel();
+        MusicListContainer.setLayout(new FlowLayout(1,100000000, 1));
+        MusicListContainer.setBackground(ColorScheme.LIGHT_BG);
+        CreateMediaList();
+
         add(title, BorderLayout.NORTH);
+        add(MusicListContainer);
+
+        // JPanel panel = new JPanel(new GridBagLayout());
+        // GridBagConstraints gbc = new GridBagConstraints();
+
+        // gbc.fill = GridBagConstraints.BOTH;
+        // gbc.gridy = 0;
+
+        // // Left (30%)
+        // gbc.gridx = 0;
+        // gbc.weightx = 0.3;
+        // panel.add(leftPanel, gbc);
+
+        // // Right (70%)
+        // gbc.gridx = 1;
+        // gbc.weightx = 0.7;
+        // panel.add(rightPanel, gbc);
     }
 }
